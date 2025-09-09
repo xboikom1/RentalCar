e0ad4885b6a40c874cc008041c081f647120e081
Maksym Boiko <Maksimboiko222@gmail.com>
2025-09-10 01:10:31 +0200

    add pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Header from "../Header/Header";
+import Loader from "../Loader/Loader";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const CatalogPage = lazy(() => import("../../pages/CatalogPage/CatalogPage"));
+const CarPage = lazy(() => import("../../pages/CarPage/CarPage"));
+const NotFoundPage = lazy(() =>
+  import("../../pages/NotFoundPage/NotFoundPage")
+);
+
 function App() {
-  return <p>HomePage</p>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/catalog/:id" element={<CarPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </div>
+  );
 }
 
 export default App;
