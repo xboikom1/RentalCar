4e2ade22b61b186dab0eb94df0ab9a2d8c5c7274
Maksym Boiko <Maksimboiko222@gmail.com>
2025-09-10 15:41:16 +0200

    update styles and button logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import css from "./HomePage.module.css";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/catalog");
+  };
+
   return (
     <section className={css.hero}>
       <div className={css.content}>
@@ -8,7 +15,9 @@ const HomePage = () => {
         <p className={css.description}>
           Reliable and budget-friendly rentals for any journey
         </p>
-        <button className={css.ctaButton}>View Catalog</button>
+        <button className={css.ctaButton} onClick={handleClick}>
+          View Catalog
+        </button>
       </div>
     </section>
   );
