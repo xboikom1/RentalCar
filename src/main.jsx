01c033f18aabd08ff174e06852655fb294e11471
Maksym Boiko <Maksimboiko222@gmail.com>
2025-09-11 19:43:06 +0200

    make adding to favourites

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { store } from "./redux/store";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
+import { store, persistor } from "./redux/store.js";
 import App from "./components/App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
-import breandsReducer from "./brands/slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "@reduxjs/toolkit";
+import brandsReducer from "./brands/slice";
 import carsReducer from "./cars/slice";
 import filtersReducer from "./filters/slice";
+import favouritesReducer from "./favourites/slice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["favourites"],
+};
+
+const rootReducer = combineReducers({
+  brands: brandsReducer,
+  cars: carsReducer,
+  filters: filtersReducer,
+  favourites: favouritesReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    brands: breandsReducer,
-    cars: carsReducer,
-    filters: filtersReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
