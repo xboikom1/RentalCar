05be5dd05b9e0161d45e1a4e000885bf88f35f1c
Maksym Boiko <Maksimboiko222@gmail.com>
2025-09-18 00:25:27 +0200

    create form component

---
+++ src/components/BookingForm/BookingForm.jsx
@@ -0,0 +1,144 @@
+import css from "./BookingForm.module.css";
+import clsx from "clsx";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { initialValues, validationSchema } from "./bookingFormConfig";
+import { useState } from "react";
+
+const BookingForm = () => {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [selected, setSelected] = useState();
+
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    const bookingData = {
+      ...values,
+      bookingDate: selected ? selected.toISOString().split("T")[0] : null,
+    };
+    console.log("Booking Data:", bookingData);
+    setTimeout(() => {
+      alert("Booking submitted successfully!");
+      resetForm();
+      setSelected();
+      setSubmitting(false);
+    }, 1000);
+  };
+
+  return (
+    <div className={css.bookingSection}>
+      <h2 className={css.bookingTitle}>Book your car now</h2>
+      <p className={css.bookingSubtitle}>
+        Stay connected. We are always ready to help you.
+      </p>
+
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting, errors, touched, setFieldValue }) => (
+          <Form className={css.bookingForm}>
+            <div className={css.fieldContainer}>
+              <Field
+                type="text"
+                name="name"
+                placeholder="Name*"
+                className={clsx(css.input, {
+                  [css.inputError]: errors.name && touched.name,
+                })}
+              />
+              <ErrorMessage
+                name="name"
+                component="span"
+                className={css.error}
+              />
+            </div>
+
+            <div className={css.fieldContainer}>
+              <Field
+                type="email"
+                name="email"
+                placeholder="Email*"
+                className={clsx(css.input, {
+                  [css.inputError]: errors.email && touched.email,
+                })}
+              />
+              <ErrorMessage
+                name="email"
+                component="span"
+                className={css.error}
+              />
+            </div>
+
+            <div className={css.fieldContainer}>
+              <div className={css.datePickerWrapper}>
+                <Field
+                  type="text"
+                  name="bookingDate"
+                  placeholder="Booking date"
+                  value={selected ? selected.toLocaleDateString() : ""}
+                  onClick={() => setShowCalendar(!showCalendar)}
+                  readOnly
+                  className={clsx(css.input, css.dateInput, {
+                    [css.inputError]: errors.bookingDate && touched.bookingDate,
+                  })}
+                />
+
+                {showCalendar && (
+                  <div className={css.calendarDropdown}>
+                    <DayPicker
+                      animate
+                      mode="single"
+                      selected={selected}
+                      onSelect={(date) => {
+                        setSelected(date);
+                        setFieldValue(
+                          "bookingDate",
+                          date ? date.toISOString().split("T")[0] : ""
+                        );
+                        setShowCalendar(false);
+                      }}
+                    />
+                  </div>
+                )}
+              </div>
+              <ErrorMessage
+                name="bookingDate"
+                component="span"
+                className={css.error}
+              />
+            </div>
+
+            <div className={css.fieldContainer}>
+              <Field
+                as="textarea"
+                name="comment"
+                placeholder="Comment"
+                className={clsx(css.textarea, {
+                  [css.inputError]: errors.comment && touched.comment,
+                })}
+              />
+              <ErrorMessage
+                name="comment"
+                component="span"
+                className={css.error}
+              />
+            </div>
+
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className={clsx(css.submitButton, {
+                [css.submitButtonDisabled]: isSubmitting,
+              })}
+            >
+              {isSubmitting ? "Sending..." : "Send"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default BookingForm;
--- src/pages/CarPage/CarPage.jsx
+++ src/pages/CarPage/CarPage.jsx
@@ -1,24 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import clsx from "clsx";
+
 import { fetchCarById } from "../../redux/cars/operations";
 import css from "./CarPage.module.css";
 import { selectCarById, selectIsLoading } from "../../redux/cars/selectors";
 import { formatMileage } from "../../utils/formatMileage";
-import { initialValues, validationSchema } from "./bookingFormConfig";
-import { DayPicker } from "react-day-picker";
-import "react-day-picker/dist/style.css";
 import ErrorComponent from "../../components/ErrorComponent/ErrorComponent";
+import BookingForm from "../../components/BookingForm/BookingForm";
 
 const CarPage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const car = useSelector(selectCarById);
   const isLoading = useSelector(selectIsLoading);
-  const [selected, setSelected] = useState();
-  const [showCalendar, setShowCalendar] = useState(false);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -26,22 +21,8 @@ const CarPage = () => {
     dispatch(fetchCarById(id));
   }, [dispatch, id]);
 
-  const handleSubmit = (values, { setSubmitting, resetForm }) => {
-    const bookingData = {
-      ...values,
-      bookingDate: selected ? selected.toISOString().split("T")[0] : null,
-    };
-    console.log("Booking Data:", bookingData);
-    setTimeout(() => {
-      alert("Booking submitted successfully!");
-      resetForm();
-      setSelected();
-      setSubmitting(false);
-    }, 1000);
-  };
-
   if (!car && !isLoading) return <ErrorComponent>Car not found</ErrorComponent>;
-  if (!car) return <div></div>;
+  if (!car) return <></>;
 
   return (
     <div className={css.pageContainer}>
@@ -52,7 +33,6 @@ const CarPage = () => {
           className={css.carImage}
         />
       </div>
-
       <div className={css.carInfo}>
         <div>
           <div className={css.header}>
@@ -72,7 +52,6 @@ const CarPage = () => {
           <p className={css.price}>${car.rentalPrice}</p>
           <p className={css.description}>{car.description}</p>
         </div>
-
         <div className={css.descriptionSections}>
           <div className={css.section}>
             <h3 className={css.sectionTitle}>Rental Conditions:</h3>
@@ -144,120 +123,7 @@ const CarPage = () => {
           </div>
         </div>
       </div>
-      <div className={css.bookingSection}>
-        <h2 className={css.bookingTitle}>Book your car now</h2>
-        <p className={css.bookingSubtitle}>
-          Stay connected. We are always ready to help you.
-        </p>
-
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          {({ isSubmitting, errors, touched, setFieldValue }) => (
-            <Form className={css.bookingForm}>
-              <div className={css.fieldContainer}>
-                <Field
-                  type="text"
-                  name="name"
-                  placeholder="Name*"
-                  className={clsx(css.input, {
-                    [css.inputError]: errors.name && touched.name,
-                  })}
-                />
-                <ErrorMessage
-                  name="name"
-                  component="span"
-                  className={css.error}
-                />
-              </div>
-
-              <div className={css.fieldContainer}>
-                <Field
-                  type="email"
-                  name="email"
-                  placeholder="Email*"
-                  className={clsx(css.input, {
-                    [css.inputError]: errors.email && touched.email,
-                  })}
-                />
-                <ErrorMessage
-                  name="email"
-                  component="span"
-                  className={css.error}
-                />
-              </div>
-
-              <div className={css.fieldContainer}>
-                <div className={css.datePickerWrapper}>
-                  <Field
-                    type="text"
-                    name="bookingDate"
-                    placeholder="Booking date"
-                    value={selected ? selected.toLocaleDateString() : ""}
-                    onClick={() => setShowCalendar(!showCalendar)}
-                    readOnly
-                    className={clsx(css.input, css.dateInput, {
-                      [css.inputError]:
-                        errors.bookingDate && touched.bookingDate,
-                    })}
-                  />
-
-                  {showCalendar && (
-                    <div className={css.calendarDropdown}>
-                      <DayPicker
-                        animate
-                        mode="single"
-                        selected={selected}
-                        onSelect={(date) => {
-                          setSelected(date);
-                          setFieldValue(
-                            "bookingDate",
-                            date ? date.toISOString().split("T")[0] : ""
-                          );
-                          setShowCalendar(false);
-                        }}
-                      />
-                    </div>
-                  )}
-                </div>
-                <ErrorMessage
-                  name="bookingDate"
-                  component="span"
-                  className={css.error}
-                />
-              </div>
-
-              <div className={css.fieldContainer}>
-                <Field
-                  as="textarea"
-                  name="comment"
-                  placeholder="Comment"
-                  className={clsx(css.textarea, {
-                    [css.inputError]: errors.comment && touched.comment,
-                  })}
-                />
-                <ErrorMessage
-                  name="comment"
-                  component="span"
-                  className={css.error}
-                />
-              </div>
-
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className={clsx(css.submitButton, {
-                  [css.submitButtonDisabled]: isSubmitting,
-                })}
-              >
-                {isSubmitting ? "Sending..." : "Send"}
-              </button>
-            </Form>
-          )}
-        </Formik>
-      </div>
+      <BookingForm />
     </div>
   );
 };
